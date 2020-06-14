4223cbb39154a3da1942509bcfb313639ace0f99
under4rest <podlesny@bokus.ru>
2020-06-14 23:39:57 +0300

    half cart and full redux

--- src/Screens/Busket/BusketScreen.js
+++ src/Screens/Busket/BusketScreen.js
@@ -1,8 +1,13 @@
 import React from "react";
-import productImg from "./../../img/complect.jpg";
+// import productImg from "./../../img/complect.jpg";
 import BusketCard from "./../../Components/BusketCard/BusketCard"
+// React
+import { connect } from 'react-redux'
 
-export default class BusketScreen extends React.Component {
+// actions
+import actionAddToCart, { actionDelFromCart } from './../../Redux/actions'
+
+class BusketScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -10,34 +15,57 @@ export default class BusketScreen extends React.Component {
       modalId: null,
     };
   }
+  sum() {
+    const { cart } = this.props
+    let price = 0
+    cart.forEach(item => {
+      price += item.price
+    });
+    return price
+  }
   render() {
+    const { cart } = this.props
     return (
       <>
         <h2>
           Корзина
         </h2>
         <div className="container">
-        <div className="busket-row">
-          <div className="row">
-          <div className="col-lg-3 offset-lg-2 col-md-3 offset-lg-2">
-            <p className="many-tovar">Количество товаров: <span className="bold">1</span></p>
-          </div>
-          <div className="col-lg-3 col-md-3">
-           <p className="sum-tovar"> Сумма цены товаров: <span className="bold">21 000р.</span></p>
-          </div>
-          <div class="col-lg-4 col-md-4">
-            <button className="btn-buy">Оплатить</button>
-          </div>
-          </div>
+          <div className="busket-row">
+            <div className="row">
+              <div className="col-lg-3 offset-lg-2 col-md-3 offset-lg-2">
+                <p className="many-tovar">Количество товаров: <span className="bold">{`${cart.length}`}</span></p>
+              </div>
+              <div className="col-lg-3 col-md-3">
+                <p className="sum-tovar"> Сумма цены товаров: <span className="bold">{`${this.sum()}`}р.</span></p>
+              </div>
+              <div className="col-lg-4 col-md-4">
+                <button className="btn-buy">Оплатить</button>
+              </div>
+            </div>
           </div>
           <p className="bold">Выбранные товары</p>
           <hr />
-          
+
         </div>
-        
+
         <BusketCard />
-        
+
       </>
     );
   }
 }
+const mapStateToPorps = (state) => {
+  return {
+    cart: state.cart,
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addToCart: (e) => dispatch(actionAddToCart(e)),
+    delFromCart: (e) => dispatch(actionDelFromCart(e))
+  }
+}
+
+export default connect(mapStateToPorps, mapDispatchToProps)(BusketScreen)
