c3158159135e3a2f130704367e81d2dfd0b7df58
ANUS KING <daniilpotolkov@gmail.com>
2020-06-26 12:41:38 +0300

    price sum fix

--- src/Components/BusketCard/BusketCard.js
+++ src/Components/BusketCard/BusketCard.js
@@ -8,34 +8,29 @@ export default class BusketScreen extends React.Component {
     };
   }
   delete() {
-    const { cart, del } = this.props
-    del(cart.product_id)
-    console.warn('press dec');
-
+    const { cart, del } = this.props;
+    del(cart.product_id);
+    console.warn("press dec");
   }
   incrise() {
-    const { cart, inc } = this.props
-    inc(cart.product_id)
-    console.warn('press inc');
-
+    const { cart, inc } = this.props;
+    inc(cart.product_id);
+    console.warn("press inc");
   }
 
   render() {
-    const { cart } = this.props
+    const { cart } = this.props;
     console.warn(cart);
 
     return (
       <>
-
         <div className="container">
           <div className="busket-row">
             <div className="row">
-
               <div className="col-lg-2 offset-lg-1">
                 <div className="busket-img">
                   <img src={cart.img} alt="" />
                 </div>
-
               </div>
               <div className="col-lg-3 ">
                 <p className="busket-name bold">{cart.name}</p>
@@ -46,15 +41,16 @@ export default class BusketScreen extends React.Component {
                 </p>
               </div>
               <div className="col-lg-2 offset-lg-1">
-                <button className="btn-add" onClick={() => this.incrise()}>Добавить</button>
-                <button className="btn-del" onClick={() => this.delete()}>Удалить</button>
+                <button className="btn-add" onClick={() => this.incrise()}>
+                  Добавить
+                </button>
+                <button className="btn-del" onClick={() => this.delete()}>
+                  Удалить
+                </button>
               </div>
             </div>
           </div>
-
-
         </div>
-
       </>
     );
   }
--- src/Screens/Busket/BusketScreen.js
+++ src/Screens/Busket/BusketScreen.js
@@ -1,11 +1,14 @@
 import React from "react";
 // import productImg from "./../../img/complect.jpg";
-import BusketCard from "./../../Components/BusketCard/BusketCard"
+import BusketCard from "./../../Components/BusketCard/BusketCard";
 // React
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
 
 // actions
-import actionAddToCart, { actionDelFromCart, actionIncCartGood } from './../../Redux/actions'
+import actionAddToCart, {
+  actionDelFromCart,
+  actionIncCartGood,
+} from "./../../Redux/actions";
 
 class BusketScreen extends React.Component {
   constructor(props) {
@@ -16,52 +19,56 @@ class BusketScreen extends React.Component {
     };
   }
   count() {
-    const { cart } = this.props
-    let cunt = 0
-    cart.forEach(item => {
-      cunt += item.good_count
+    const { cart } = this.props;
+    let cunt = 0;
+    cart.forEach((item) => {
+      cunt += item.good_count;
     });
-    return cunt
+    return cunt;
   }
   sum() {
-    const { cart } = this.props
-    let price = 0
-    cart.forEach(item => {
-      price += item.price
+    const { cart } = this.props;
+    let price = 0;
+    cart.forEach((item) => {
+      price += item.price * item.good_count;
     });
-    return price
+    return price;
   }
   render() {
-    const { cart, delFromCart, incCartGood } = this.props
+    const { cart, delFromCart, incCartGood } = this.props;
     return (
       <>
-      <div className="wrap">
-        <h2>
-          Корзина
-        </h2>
-        <div className="container">
-          <div className="busket-row">
-            <div className="row">
-              <div className="col-lg-3 offset-lg-2 col-md-3 offset-lg-2">
-                <p className="many-tovar">Количество товаров: <span className="bold">{`${this.count()}`}</span></p>
-              </div>
-              <div className="col-lg-3 col-md-3">
-                <p className="sum-tovar"> Сумма цены товаров: <span className="bold">{`${this.sum()}`}р.</span></p>
-              </div>
-              <div className="col-lg-4 col-md-4">
-                <button className="btn-buy">Оплатить</button>
+        <div className="wrap">
+          <h2>Корзина</h2>
+          <div className="container">
+            <div className="busket-row">
+              <div className="row">
+                <div className="col-lg-3 offset-lg-2 col-md-3 offset-lg-2">
+                  <p className="many-tovar">
+                    Количество товаров:{" "}
+                    <span className="bold">{`${this.count()}`}</span>
+                  </p>
+                </div>
+                <div className="col-lg-3 col-md-3">
+                  <p className="sum-tovar">
+                    {" "}
+                    Сумма цены товаров:{" "}
+                    <span className="bold">{`${this.sum()}`}р.</span>
+                  </p>
+                </div>
+                <div className="col-lg-4 col-md-4">
+                  <button className="btn-buy">Оплатить</button>
+                </div>
               </div>
             </div>
+            <p className="bold">Выбранные товары</p>
+            <hr />
           </div>
-          <p className="bold">Выбранные товары</p>
-          <hr />
-
-        </div>
-        {
-          cart.map((item, i) => {
-            return <BusketCard cart={item} del={delFromCart} inc={incCartGood} />
-          })
-        }
+          {cart.map((item, i) => {
+            return (
+              <BusketCard cart={item} del={delFromCart} inc={incCartGood} />
+            );
+          })}
         </div>
       </>
     );
@@ -70,15 +77,15 @@ class BusketScreen extends React.Component {
 const mapStateToPorps = (state) => {
   return {
     cart: state.cart,
-  }
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     addToCart: (e) => dispatch(actionAddToCart(e)),
     delFromCart: (e) => dispatch(actionDelFromCart(e)),
-    incCartGood: (e) => dispatch(actionIncCartGood(e))
-  }
-}
+    incCartGood: (e) => dispatch(actionIncCartGood(e)),
+  };
+};
 
-export default connect(mapStateToPorps, mapDispatchToProps)(BusketScreen)
+export default connect(mapStateToPorps, mapDispatchToProps)(BusketScreen);
